00c2a417c94f90907590f38a9df1b5f1d0bf6e04
esplusnin <esplusnin94@yandex.ru>
2025-11-05 10:55:16 +0300

    Initial Commit

---
+++ RareRed Wings/RareRed_WingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RareRed_WingsApp.swift
+//  RareRed Wings
+//
+//  Created by Евгений on 05.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct RareRed_WingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
